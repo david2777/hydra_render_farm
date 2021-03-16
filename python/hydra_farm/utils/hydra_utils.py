9b9021aa4d7222db7468eb8d32960bc0b1948b63
david2777 <daduvo11@gmail.com>
2021-03-15 22:06:01 -0700

    + Add docstrings to Timer

--- python/hydra_farm/utils/hydra_utils.py
+++ python/hydra_farm/utils/hydra_utils.py
@@ -10,17 +10,40 @@ config = yaml_cache.get_hydra_cfg()
 
 
 class Timer(object):
+    """Simple Timer"""
     def __init__(self, name: str = None, logger: logging.Logger = None, log_level: str = 'debug'):
+        """Simple Timer
+
+        Args:
+            name (str): Name to log, default "Process".
+            logger (logging.Logger): Logger to log results to. Default None will print instead.
+            log_level (str): Log level as a string, eg "debug", "info", etc. Default "debug".
+
+        """
         self.name = name if name is not None else 'Process'
         self.logger = logger
         self.log_level = log_level
+        if self.logger and not hasattr(self.logger, self.log_level):
+            raise ValueError(f'Logger {self.logger} has no level {self.log_level}')
         self.start = time.perf_counter()
 
     def log(self, name: str = None, log_level: str = None, precision: int = 3) -> str:
-        name = name if name is not None else self.name
+        """Log the result to the logger.
+
+        Args:
+            name (str): Name to log, default from self.name.
+            log_level (str): Log level as a string, eg "debug", "info", etc. Default "debug".
+            precision (int): Floating point precision for the timer, default 3.
+
+        Returns:
+            str: The log statement
+
+        """
         log_level = log_level if log_level is not None else self.log_level
+        if self.logger and not hasattr(self.logger, log_level):
+            raise ValueError(f'Logger {self.logger} has no level {self.log_level}')
         elapsed = self.end(precision)
-        log = '{0} took {1} seconds'.format(name, elapsed)
+        log = f'{name if name is not None else self.name} took {elapsed} seconds'
         if self.logger:
             getattr(self.logger, log_level)(log)
         else:
@@ -28,6 +51,15 @@ class Timer(object):
         return log
 
     def end(self, precision: int = 3) -> str:
+        """Return the timer value as a string.
+
+        Args:
+            precision (int): Floating point precision for the timer, default 3.
+
+        Returns:
+            str: The timer value as a string, truncated to the precision value
+
+        """
         return '%.{}f'.format(precision) % (time.perf_counter() - self.start)
 
 
